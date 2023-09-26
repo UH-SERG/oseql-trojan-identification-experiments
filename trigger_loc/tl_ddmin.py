ebdb0d6d0f021caad69b5de30aa4108ef8ad1c2e
root <aftab.hussain46@gmail.com>
2023-09-26 21:58:39 +0000

    ddmin-based-trig-loc: handle case for no cand trigg

--- trigger_loc/tl_ddmin.py
+++ trigger_loc/tl_ddmin.py
@@ -8,16 +8,16 @@ import string
 import sys
 import json
 
-EVAL_FN = None
-EVAL_FN_DEFAULT_ARGS = None
-CODE_CHUNKS = []
-CODE_CHUNK_IDS = []
-CANDIDATE_TRIGGER_ID = -1
-CANDIDATE_TRIGGER_CONFIDENCE = 0.0
-OUTPUT_DIR = ""
-DDMIN_LOG = ""
-LOG_BREAK="************************************************************************\n"
-ITER_NUM=0
+eval_fn = None
+eval_fn_default_args = None
+code_chunks = []
+code_chunk_ids = []
+candidate_trigger_id = -1
+candidate_trigger_prob = 0.0
+output_dir = ""
+ddmin_log = ""
+LOG_BREAK="*"*50 + "\n"
+iter_num=0
 
 max_similarity = -1.0
 max_similarity_test_case_id = -1
@@ -35,53 +35,53 @@ class MyDD(DD.DD):
     def coerce(self, deltas):
         removed_code = ""
         for chunk_id in deltas:
-          removed_code = "".join(CODE_CHUNKS[chunk_id-1])
+          removed_code = "".join(code_chunks[chunk_id-1])
         return removed_code
 
     def __init__(self):
         DD.DD.__init__(self)
         
     def _test(self, deltas):
-        global CANDIDATE_TRIGGER_ID, CANDIDATE_TRIGGER_CONFIDENCE, ITER_NUM
+        global candidate_trigger_id, candidate_trigger_prob, iter_num
 
         if len(deltas)==0:
            print("No code to remove, ignore.")
-           for chunk_id in CODE_CHUNK_IDS:
-              json.dump({chunk_id: CODE_CHUNKS[chunk_id-1]},DDMIN_LOG)
-              DDMIN_LOG.write('\n')
+           for chunk_id in code_chunk_ids:
+              json.dump({chunk_id: code_chunks[chunk_id-1]},ddmin_log)
+              ddmin_log.write('\n')
            return self.PASS
-        if len(deltas)==len(CODE_CHUNKS):
+        if len(deltas)==len(code_chunks):
            print("Remove all code, ignore.")
-           return self.FAIL
+           return self.FAIL # Switch PASS, FAIL will cause further minimization
 
         
-        DDMIN_LOG.write(LOG_BREAK)
-        DDMIN_LOG.write(f"DDMIN LINES ITERATION NUMBER : {ITER_NUM}\n")
-        ITER_NUM+=1
-        DDMIN_LOG.write(LOG_BREAK)
-        DDMIN_LOG.write("MODIFIED CODE:\n")
+        ddmin_log.write(LOG_BREAK)
+        ddmin_log.write(f"DDMIN LINES ITERATION NUMBER : {iter_num}\n")
+        iter_num+=1
+        ddmin_log.write(LOG_BREAK)
+        ddmin_log.write("MODIFIED CODE:\n")
         modified_code_dict = {}
-        for chunk_id in CODE_CHUNK_IDS:
+        for chunk_id in code_chunk_ids:
             if chunk_id not in deltas:
-              modified_code_dict[chunk_id] = CODE_CHUNKS[chunk_id-1]
-              json.dump({chunk_id: CODE_CHUNKS[chunk_id-1]},DDMIN_LOG)
-              DDMIN_LOG.write('\n')
+              modified_code_dict[chunk_id] = code_chunks[chunk_id-1]
+              json.dump({chunk_id: code_chunks[chunk_id-1]},ddmin_log)
+              ddmin_log.write('\n')
 
-        DDMIN_LOG.write(LOG_BREAK)
-        DDMIN_LOG.write("REMOVED CODE:\n")
+        ddmin_log.write(LOG_BREAK)
+        ddmin_log.write("REMOVED CODE:\n")
         for chunk_id in deltas:
-              json.dump({chunk_id: CODE_CHUNKS[chunk_id-1]},DDMIN_LOG)
-              DDMIN_LOG.write('\n')
+              json.dump({chunk_id: code_chunks[chunk_id-1]},ddmin_log)
+              ddmin_log.write('\n')
         
-        DDMIN_LOG.write(LOG_BREAK)
-        DDMIN_LOG.write("MODEL OUTPUT ON MODIFIED CODE (prediction, confidence):\n")
-        pred, confidence = EVAL_FN(modified_code_dict,*EVAL_FN_DEFAULT_ARGS)
-        DDMIN_LOG.write(f"{pred}, {confidence:.4f}\n")
+        ddmin_log.write(LOG_BREAK)
+        ddmin_log.write("MODEL OUTPUT ON MODIFIED CODE (prediction, prob):\n")
+        pred, prob = eval_fn(modified_code_dict,*eval_fn_default_args)
+        ddmin_log.write(f"{pred}, {prob:.4f}\n")
 
         if pred == 1:
             # The prediction of the model becomes 1.
-            CANDIDATE_TRIGGER_ID = deltas[0]
-            CANDIDATE_TRIGGER_CONFIDENCE = confidence 
+            candidate_trigger_id = deltas[0]
+            candidate_trigger_prob = prob 
             return self.FAIL
         else:
             return self.PASS
@@ -89,19 +89,23 @@ class MyDD(DD.DD):
 def get_trigger_ddmin_lines(code_lines, log, eval_fn, eval_fn_default_args):
   print("Start")
   print(code_lines)
-  global CODE_CHUNKS, EVAL_FN, EVAL_FN_DEFAULT_ARGS, CODE_CHUNK_IDS, DDMIN_LOG
-  CODE_CHUNKS = code_lines
-  EVAL_FN = eval_fn
-  EVAL_FN_DEFAULT_ARGS = eval_fn_default_args
-  DDMIN_LOG = log
+  global code_chunks, eval_fn, eval_fn_default_args, code_chunk_ids, ddmin_log, candidate_trigger_id
+  candidate_trigger_id = -1
+  code_chunks = code_lines
+  eval_fn = eval_fn
+  eval_fn_default_args = eval_fn_default_args
+  ddmin_log = log
 
-  CODE_CHUNK_IDS = list(range(1, len(code_lines)+1))
-  deltas = CODE_CHUNK_IDS
+  code_chunk_ids = list(range(1, len(code_lines)+1))
+  deltas = code_chunk_ids
 
   mydd = MyDD()
   
   c = mydd.ddmin(deltas)              # Invoke DDMIN
 
-  return CANDIDATE_TRIGGER_ID, CANDIDATE_TRIGGER_CONFIDENCE
+  if candidate_trigger_id == -1:
+      return None
+
+  return candidate_trigger_id, candidate_trigger_prob
 
 
