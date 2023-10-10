7bb993d93ca1682445bad151a30358501b92fd0f
Aftab Hussain <aftab.hussain46@gmail.com>
2023-10-10 14:15:27 -0500

    add outlier methods test script

---
+++ test_outlier_methods.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+: <<'COMMENT'
+
+python3 test_outlier_methods.py \
+	--folder1 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/codebert/codebert_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/clean-examples/sample-logs/ \
+	--folder2 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/codebert/codebert_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/model-tricking-examples/sample-logs/ \
+	--model codebert  > codebert-chk
+
+python3 test_outlier_methods.py \
+	--folder1 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/codet5_small/codet5_small_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/clean-examples/sample-logs/ \
+	--folder2 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/codet5_small/codet5_small_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/model-tricking-examples/sample-logs \
+	--model codet5  > codet5-chk
+
+python3 test_outlier_methods.py \
+	--folder1 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/roberta/roberta_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/clean-examples/sample-logs \
+	--folder2 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/roberta/roberta_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/model-tricking-examples/sample-logs \
+	--model roberta  > roberta-chk
+
+COMMENT
+
+python3 test_outlier_methods.py \
+	--folder1 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/bart_base/bart_base_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/clean-examples/sample-logs \
+	--folder2 /scratch1/aftab/CodeT5-original-gpu0/CodeT5/sh/saved_models/defect/1-to-0_poisoning/DCI_pr2/bart_base/bart_base_all_lr1_bs16_src512_trg3_pat2_e50/trigger_loc_expt/seq-lines/model-tricking-examples/sample-logs \
+	--model bart  > bart-chk
